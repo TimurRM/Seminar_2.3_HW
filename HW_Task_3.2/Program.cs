7e094d083b22ea4e5aac4048e0f128d8db1eb7c2
Timur <tumyp.paxumob@gmail.com>
2022-07-02 20:32:37 +0500

    Added HW_Task_3.2

--- HW_Task_3.2/Program.cs
+++ HW_Task_3.2/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+// double SystemCoor(int x1, int y1, int z1, int x2, int y2, int z2){
+//     return Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2) ,2) + Math.Pow((z1 - z2), 2));
+// }
+
+//                 Console.WriteLine("Введите координату x1");
+//                 int Ax = Convert.ToInt32(Console.ReadLine());
+//                 Console.WriteLine("Введите координату y1");
+//                 int Ay = Convert.ToInt32(Console.ReadLine());
+//                 Console.WriteLine("Введите координату z1");
+//                 int Az = Convert.ToInt32(Console.ReadLine());
+//                 Console.WriteLine("Введите координату x2");
+//                 int Bx = Convert.ToInt32(Console.ReadLine());
+//                 Console.WriteLine("Введите координату y2");
+//                 int By = Convert.ToInt32(Console.ReadLine());
+//                 Console.WriteLine("Введите координату z2");
+//                 int Bz = Convert.ToInt32(Console.ReadLine());
+
+//         Console.WriteLine(Math.Round(SystemCoor(Ax, Ay, Az, Bx, By, Bz), 2));
+
+
+
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите координату Ax");
+        int Ax = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите координату Ay");
+        int Ay = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите координату Az");
+        int Az = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите координату Bx");
+        int Bx = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите координату By");
+        int By = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите координату Bz");
+        int Bz = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("A " + (Ax,Ay,Az) + "; B" + (Bx,By,Bz) + " -> ");
+        Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow(Ax - Bx, 2) + Math.Pow(Ay - By, 2) + Math.Pow(Az - Bz, 2)), 2));
+    }
+}
