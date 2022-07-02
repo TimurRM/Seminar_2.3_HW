372db1d2d6f376d56ba0a09f8ad1f2b74e18f81f
Timur <tumyp.paxumob@gmail.com>
2022-07-02 15:36:01 +0500

    Added files

--- HW_Task_3.1/Program.cs
+++ HW_Task_3.1/Program.cs
@@ -1,23 +1,32 @@
-﻿void palindrom()
+﻿internal class Program
+{
+    private static void Main(string[] args)
     {
-        int number = new Random().Next(10, 999999);
-        int num1 = number / 10000 % 10;
-        int num2 = number / 1000 % 10;
-        int rev1 = number / 10 % 10;
-        int rev2 = number % 10;
-        int nul = number / 10000;
-        
-            if (nul < 1 || nul > 9){
-            Console.WriteLine(number + " -> " + "Not 5-digit number");
+        void palindrom()
+        {
+            int number = new Random().Next(10, 999999);
+            int num1 = number / 10000 % 10;
+            int num2 = number / 1000 % 10;
+            int rev1 = number / 10 % 10;
+            int rev2 = number % 10;
+            int nul = number / 10000;
+
+            if (nul < 1 || nul > 9)
+            {
+                Console.WriteLine(number + " -> " + "Not 5-digit number");
             }
-            else if  (num1 == rev2 && num2 == rev1){
-            Console.WriteLine(number + " -> Yes, this is palindrom");
+            else if (num1 == rev2 && num2 == rev1)
+            {
+                Console.WriteLine(number + " -> Yes, this is palindrom");
             }
-            else{
-            Console.WriteLine(number + " -> No, this is not palindrom");
+            else
+            {
+                Console.WriteLine(number + " -> No, this is not palindrom");
             }
         }
         for (int i = 0; i < 100; i++)
         {
             palindrom();
+        }
     }
+}
